678e40ac9ced1689407cdc0d459ed1fbf0f5cadf
Nathan Green <nathan.green@jet2.com>
2018-08-06 11:16:36 +0100

    added mocha tests

--- readme.md
+++ readme.md
@@ -2,32 +2,38 @@
 
 Basic helper functions for percentage based calculations
 
-### as()
+### Installation
+
+`npm install percent-calc`
+
+`import percent from 'percent-calc'` or `const percent = require('percent-calc')`
+
+------
+
+### Usage
+
+#### as()
 
 What is `num1` as a percentage of `num2`
 
 What is `20` as a percentage of `80`
 
-```
-percent.as(20, 80) // 25.00
-```
+`percent.as(20, 80) // 25.00`
 
 Params:
 * num1
 * num2
-* decimal (default 2)
+* decimal (optional, default 2)
 
-### of()
+#### of()
 
 What is `num1`% of `num2`
 
 What is `23`% of `62`
 
-```
-percent.of(23, 62) // 14.26
-```
+`percent.of(23, 62) // 14.26`
 
 Params:
 * num1
 * num2
-* decimal (default 2)
+* decimal (optional, default 2)
--- test/test.js
+++ test/test.js
@@ -1,22 +1,36 @@
 const assert = require('chai').assert
 const percent = require('../index')
 
-describe('Percentage functions', function () {
-  it('Percent of', function () {
+describe('as', function () {
+  it('40 as a percent of 60', function () {
+    let result = percent.as(40, 60)
+    assert.equal(result, 66.67)
+  })
+
+  it('55 as a percent of 65', function () {
+    let result = percent.as(55, 65)
+    assert.equal(result, 84.62)
+  })
+
+  it('three decimal', function () {
+    let result = percent.as(55, 65, 3)
+    assert.equal(result, 84.615)
+  })
+})
+
+describe('of', function () {
+  it('40% of 60', function () {
     let result = percent.of(40, 60)
-    assert.equal(result, 24)
-    assert.typeOf(result, 'number')
+    assert.equal(result, 24.00)
   })
 
-  it('As percent of', function () {
-    let result = percent.as(30, 60)
-    assert.equal(result, 50)
-    assert.typeOf(result, 'number')
+  it('55% of 65', function () {
+    let result = percent.of(55, 65)
+    assert.equal(result, 35.75)
   })
 
-  it('As percent of (with symbol)', function () {
-    let result = percent.as(30, 60, true)
-    assert.equal(result, '50%')
-    assert.typeOf(result, 'string')
+  it('three decimal', function () {
+    let result = percent.of(31, 33, 3)
+    assert.equal(result, 10.230)
   })
 })
